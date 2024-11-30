728c1e59cafa386306b1cd4e884dc736b4a4ea42
Pedro Raiol <phr.fernandes04@gmail.com>
2024-11-30 11:21:33 -0400

    Funções

--- src/fundamentos/Funcao.kt
+++ src/fundamentos/Funcao.kt
@@ -1,2 +1,29 @@
 package fundamentos
 
+fun soma(x: Int, y: Int): Int{
+    return x + y
+}
+
+fun subtracao(a: Int, b: Int) = a - b // Função de uma unica linha
+
+fun imprimir_msg(){
+    println("Ola, Mundo")
+}
+
+fun saudar(nome: String = "Visitante"){ // Função com valor padrão no paramento
+    println("Ola, $nome")
+}
+
+fun main() {
+    println(soma(5, 2))
+    println(subtracao(10, 5))
+    imprimir_msg()
+    saudar()
+    saudar("Pedro")
+
+    // Função Lambda
+    val multicador = {p: Int, h: Int -> p * h}
+    println(multicador(4, 5))
+
+
+}
